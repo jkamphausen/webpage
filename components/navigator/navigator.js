fa89de2057818141f027c5c59c0a786ce431d5be
jkamphausen <juliankamphausen@me.com>
2020-09-29 15:25:44 +0200

    clean up
    clean everthing up

--- components/navigator/navigator.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const Navigator = styled.footer`
-    font-size: 4.236em;
-    bottom: 0;
-`
--- components/signet/signet.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Signet = styled.div`
-  grid-column: 1 / 1;
-  grid-row: 1 / 1; 
-  display: grid;
-  justify-items: center;
-  align-items: center;
-`
