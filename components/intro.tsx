962c40e9528d96179fbeaf4708b436fe1a10db69
Julian Kamphausen <juliankamphausen@me.com>
2021-01-11 22:54:45 +0100

    fixes

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,20 +1,33 @@
-import { CMS_NAME } from '../lib/constants'
+import { CMS_NAME, PAGE_NAME } from '../lib/constants'
+import Link from 'next/link'
 
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        jkamphausen.io
-      </h1>
+      <span className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        {PAGE_NAME}
+      </span>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        {/* A statically generated blog example using{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-success duration-200 transition-colors"
         >
           Next.js
         </a>{' '}
-        and {CMS_NAME}.
+        and {CMS_NAME}. */}
+
+        <Link href="/">
+          <a className="hover:underline">Home</a>
+        </Link>
+        <span> | </span>
+        <Link href="/about">
+          <a className="hover:underline">About</a>
+        </Link>
+        <span> | </span>
+        <Link href="/projects">
+          <a className="hover:underline">Projects</a>
+        </Link>
       </h4>
     </section>
   )
---
+++ pages/about.tsx
@@ -0,0 +1,44 @@
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../types/post'
+
+type Props = {
+    allPosts: Post[]
+}
+
+const Index = ({ allPosts }: Props) => {
+    const heroPost = allPosts[0]
+    const morePosts = allPosts.slice(1)
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>About | jkamphausen.io</title>
+                </Head>
+                <Container>
+                    <Intro />
+                    <h1 className="text-6xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-6">About</h1>
+                    <br></br>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    <br></br>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    <br></br>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </Container>
+            </Layout>
+        </>
+    )
+}
+
+export default Index
+
+export const getStaticProps = async () => {
+    return {
+        props: {},
+    }
+}
